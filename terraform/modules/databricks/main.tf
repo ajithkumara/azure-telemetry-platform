835876ad06c7c285d38a9e2ae4bc37b698f0a1a9
Ajith Kumara <aakumara@gmail.com>
2026-07-11 18:37:29 -0400

    Fix Databricks Premium SKU and storage account naming

--- terraform/modules/databricks/main.tf
+++ terraform/modules/databricks/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_databricks_workspace" "databricks" {
   name                = "dbw-${var.name_suffix}"
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "standard"
+  sku                 = "premium"
 
   tags = var.tags
 }
